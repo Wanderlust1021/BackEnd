53b62dff7ba0596e12a80fda39c1fc271c40fc53
Ty Lippe <tylippe94@gmail.com>
2019-10-19 11:59:02 -0400

    file framework and depends added

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+module.exports = {
+    development: {
+      client: 'sqlite3',
+      connection: { filename: './database/auth.db3' }, 
+      useNullAsDefault: true,
+      migrations: {
+        directory: './database/migrations',
+        tableName: 'dbmigrations',
+      },
+      seeds: { directory: './database/seeds' },
+    },
+  };
+  
