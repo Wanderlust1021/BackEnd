1d91c76dfd968fd7a140a2ad2154f17f41c2d624
Ty Lippe <tylippe94@gmail.com>
2019-10-19 12:09:39 -0400

    server.js added, added to cofig folder, working on models

---
+++ config/model.js
@@ -0,0 +1,16 @@
+const db = require('../database/dbConfig')
+
+module.exports = {
+    addUser,
+    findUser,
+    findUserById,
+    addOrg,
+    findOrg,
+    findOrgById,
+    addExp,
+    editExp,
+    deleteExp,
+    findExp,
+    addImg
+};
+
