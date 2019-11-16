ab1fb2205cc8dc4067d24c94c28dddb4819df339
Roger Lee <rmlee0003@gmail.com>
2019-11-16 11:40:18 -0500

    empty

--- index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-const server = require('./api/server.js');
-
-const port = process.env.PORT || 5000;
-server.listen(port, () => {
-    console.log(`\n=== Server listening on port ${port}\n`)
-})
